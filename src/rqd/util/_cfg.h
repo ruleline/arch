fed543c618dd4a3d3e45dea8a293d7b1f75bf8ca
ruleline <ruleline@outlook.com>
2024-05-05 17:58:31 +0800

    添加配置文件
    (cherry picked from commit 290427d40d570b9749b311515d3b5845d5ed4637)

---
+++ src/rqd/util/_cfg.h
@@ -0,0 +1,32 @@
+/**
+ * @file _cfg.h
+ * @brief _cfg
+ * @author ruleline (ruleline@outlook.com)
+ * @since 2024-03-24
+ *
+ * @authors ruleline (ruleline@outlook.com)
+ * @date 2024-05-03
+ * @version 0.00.001
+ *
+ * @copyright ©2024 ruleline
+ *
+ * @details
+ * -----------------------------------------------------------------------------
+ *    version   |    date    |       by       |              comments
+ * ------------ | ---------- | -------------- | --------------------------------
+ *   0.00.001   | 2024-01-10 |    ruleline    | 初版
+ * -----------------------------------------------------------------------------
+ */
+
+#ifndef _CFG_H
+#define _CFG_H
+
+#define _C99
+#define _ARM
+#define _NDEBUG
+// #define _FREERTOS
+
+#define _INIT_START (0)
+#define _INIT_END   (1)
+
+#endif /* _CFG_H */
